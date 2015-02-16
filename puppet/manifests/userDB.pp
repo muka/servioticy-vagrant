ace37cb15acc0761e743c13fec71fbb1ce518d29
Luca Capra <luca.capra@gmail.com>
2015-02-16 12:06:54 +0100

    symlink userDB on /data to allow access on shared directory from host

--- puppet/manifests/userDB.pp
+++ puppet/manifests/userDB.pp
@@ -1,18 +1,25 @@
+
+#file { '/data/userDB':
+#          ensure => directory,
+#          replace => true,
+#          owner    => 'vagrant',
+#          group    => 'vagrant',
+#          source => "/vagrant/puppet/files/userDB",
+#          recurse => remote,
+#          before     => Exec['run_userDB']
+#}
+
+# use a symlink to /data/userDB/users.db to allow external access to VM users.db
 file { '/data/userDB':
-          ensure => directory,
-          replace => true,
-          owner    => 'vagrant',
-          group    => 'vagrant',          
-          source => "/vagrant/puppet/files/userDB",
-          recurse => remote,
-          before     => Exec['run_userDB']
+   ensure => 'link',
+   target => '/vagrant/puppet/files/userDB',
 }
 
 exec { 'run_userDB':
     require => [ Package['python-pip'], File['/data/userDB']],
     user    => 'vagrant',
     group    => 'vagrant',
-    unless => "ps -fA | grep userDB | grep -v grep",          
+    unless => "ps -fA | grep userDB | grep -v grep",
     cwd => "/data/userDB/",
     path => "/bin:/usr/bin/",
     command => "python userDB.py &"
