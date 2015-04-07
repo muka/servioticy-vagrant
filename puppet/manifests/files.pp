0280fb4e42b0268a3a14e9485225d2c00b0af9bf
Luca Capra <luca.capra@gmail.com>
2015-04-07 17:55:52 +0200

    review merge

--- puppet/manifests/files.pp
+++ puppet/manifests/files.pp
@@ -1,4 +1,4 @@
-file { '/home/vagrant/downloads/':
+file { '/tmp/servioticy/':
   ensure => 'directory',
 }
 
@@ -15,70 +15,93 @@ file { '/data/couchbase':
 
 file { '/opt/apache-storm-0.9.2-incubating':
           ensure => 'directory',
-          owner => 'servioticy',
-          group => 'servioticy',
+          owner => 'vagrant',
+          group => 'vagrant',
 #          before => Exec['run_storm']
 }
+
+group { "tomcat7":
+      ensure => present,
+} ->
+user { "tomcat7":
+    ensure => present,
+    groups => 'tomcat7',
+} ->
+file { '/etc/tomcat7':
+  ensure => 'directory',
+  owner => "tomcat7",
+  group => "tomcat7",
+} ->
+file { '/etc/tomcat7/server.xml':
+          ensure => present,
+          replace => true,
+          owner    => 'tomcat7',
+          group    => 'tomcat7',
+          before => Package['tomcat7'],
+          source => "/opt/servioticy-vagrant/puppet/files/server.xml",
+}
+
+
 
 file { '/home/servioticy/LICENSE.txt':
           ensure => present,
           replace => true,
-          owner    => 'servioticy',
-          group    => 'servioticy',
+          owner    => 'vagrant',
+          group    => 'vagrant',
           source => "/opt/servioticy-vagrant/puppet/files/other/LICENSE.txt",
 }
 
 file { '/home/servioticy/README.txt':
           ensure => present,
           replace => true,
-          owner    => 'servioticy',
-          group    => 'servioticy',
+          owner    => 'vagrant',
+          group    => 'vagrant',
           source => "/opt/servioticy-vagrant/puppet/files/other/README.txt",
 }
 
 file { '/home/servioticy/README.demos.txt':
           ensure => present,
           replace => true,
-          owner    => 'servioticy',
-          group    => 'servioticy',
+          owner    => 'vagrant',
+          group    => 'vagrant',
           source => "/opt/servioticy-vagrant/puppet/files/other/README.demos.txt",
 }
 
 file { '/home/servioticy/VERSION.txt':
           ensure => present,
           replace => true,
-          owner    => 'servioticy',
-          group    => 'servioticy',
+          owner    => 'vagrant',
+          group    => 'vagrant',
           source => "/opt/servioticy-vagrant/puppet/files/other/VERSION.txt",
 }
 
 file { '/opt/servioticy-dispatcher':
           ensure => 'directory',
-          owner => 'servioticy',
-          group => 'servioticy'
+          owner => 'vagrant',
+          group => 'vagrant'
 }
 
 file { '/opt/servioticy-dispatcher/dispatcher-0.4.3-security-SNAPSHOT-jar-with-dependencies.jar':
           ensure => present,
           source => "/usr/src/servioticy/servioticy-dispatcher/target/dispatcher-0.4.3-security-SNAPSHOT-jar-with-dependencies.jar",
           require => [File['/opt/servioticy-dispatcher'],Exec['build_servioticy'],File['/opt/servioticy-dispatcher']],
-          owner => 'root',
-          group => 'root'
+          owner => 'vagrant',
+          group => 'vagrant'
 }
 
 file { '/opt/servioticy-dispatcher/dispatcher.xml':
           ensure => present,
           source => "/opt/servioticy-vagrant/puppet/files/dispatcher.xml",
           require => [File['/opt/servioticy-dispatcher'], Exec['build_servioticy'],File['/opt/servioticy-dispatcher']],
-          owner => 'root',
-          group => 'root'
+          owner => 'vagrant',
+          group => 'vagrant'
 }
 
 file { '/data/demo':
           ensure => directory,
           replace => true,
-          owner    => 'servioticy',
-          group    => 'servioticy',
+          owner    => 'vagrant',
+          group    => 'vagrant',
           source => "/opt/servioticy-vagrant/puppet/files/demo",
           recurse => remote
 }
@@ -87,8 +110,8 @@ file { '/data/demo':
 file { '/opt/servioticy_scripts':
           ensure => directory,
           replace => true,
-          owner    => 'servioticy',
-          group    => 'servioticy',
+          owner    => 'vagrant',
+          group    => 'vagrant',
           source => "/opt/servioticy-vagrant/puppet/scripts",
           recurse => remote
 }
@@ -107,6 +130,23 @@ file { '/opt/jetty/webapps/root.war':
           notify  => Service["jetty"],
           require => Exec['build_servioticy']
 }
+
+
+package {'ant':
+  ensure => present,
+  require=> [ Package['oracle-java7-installer'],Exec['apt-get update'] ],
+}
+
+package {'libmysql-java':
+  ensure => present,
+  require=> [ Package['oracle-java7-installer'],Exec['apt-get update'] ],
+}
+
+package {'tomcat7':
+  ensure => present,
+  require=> [ Package['oracle-java7-installer'],Exec['apt-get update'] ],
+}
+
 
 file { '/var/lib/tomcat7/webapps/uaa.war':
           ensure => present,
@@ -133,7 +173,7 @@ file { '/opt/compose-idm':
           ensure => 'directory',
           owner => 'vagrant',
           group => 'vagrant'
-} 
+}
 
 
 file { '/opt/compose-idm/COMPOSEIdentityManagement-0.8.0.jar':
@@ -145,7 +185,7 @@ file { '/opt/compose-idm/COMPOSEIdentityManagement-0.8.0.jar':
 
 file { '/usr/src/compose-idm/src/main/resources/uaa.properties':
           ensure => present,
-          source => "/vagrant/puppet/files/idm/uaa.properties",
+          source => "/opt/servioticy-vagrant/puppet/files/idm/uaa.properties",
           require => [ File['/opt/compose-idm'] ]
 }
 
@@ -153,7 +193,7 @@ file { '/usr/src/compose-idm/src/main/resources/uaa.properties':
 
 file { '/tmp/mysql-server.response':
           ensure => present,
-          source => "/vagrant/puppet/files/mysql-server.response",
+          source => "/opt/servioticy-vagrant/puppet/files/mysql-server.response",
 }
 
 
@@ -163,7 +203,7 @@ file { '/usr/share/tomcat7/lib/mysql-connector-java-5.1.16.jar':
           require => Package['libmysql-java', 'tomcat7'],
 }
 
-file { '/home/vagrant/.bash_aliases':
+file { '/home/servioticy/.bash_aliases':
    ensure => 'link',
-   target => '/vagrant/puppet/scripts/env.sh',
+   target => '/opt/servioticy-vagrant/puppet/scripts/env.sh',
 }
