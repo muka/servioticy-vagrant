84f5d4a318960cd3cb9dff3a5a7d55bd977b5f5d
Luca Capra <luca.capra@gmail.com>
2015-04-21 13:02:59 +0200

    fixed module linking

--- scripts/puppet-standalone.sh
+++ scripts/puppet-standalone.sh
@@ -2,7 +2,7 @@
 
 sudo sh ./scripts/prepare_env.sh
 
-sudo ln -s $PWD/puppet/modules/compose-servioticy /etc/puppet/modules/
+sudo ln -s $PWD/puppet/modules/compose-servioticy /etc/puppet/modules/servioticy
 
 sudo puppet apply \
 --debug \
