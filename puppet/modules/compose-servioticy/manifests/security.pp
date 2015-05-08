eae2550dff68aeeaac6f6da2a6d1b73ab18ff290
Luca Capra <luca.capra@gmail.com>
2015-05-08 23:59:54 +0200

    fix idm install

--- puppet/modules/compose-servioticy/manifests/security.pp
+++ puppet/modules/compose-servioticy/manifests/security.pp
@@ -21,7 +21,7 @@ class servioticy::security {
     exec { "build compose-idm":
         path    => "/usr/local/bin/:/usr/bin:/bin/:${servioticy::params::srcdir}/compose-idm:${servioticy::params::gradle_path}",
         cwd     => "${servioticy::params::srcdir}/compose-idm",
-        command => "gradle installApp",
+        command => "gradle installApp -x test",
         user    => "root",
         group   => "root",
     } ->
